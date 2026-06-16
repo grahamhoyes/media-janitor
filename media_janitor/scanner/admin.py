5c36df9c2c9fbbe7761e1fb6cc5a08ea19fe094b
Graham Hoyes <accounts@grahamhoyes.com>
2026-06-16 16:24:13 -0400

    Add models for scan data

--- media_janitor/scanner/admin.py
+++ media_janitor/scanner/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Config
+from .models import Blob, Config, Scan
 
 admin.site.register(Config)
+admin.site.register(Scan)
+admin.site.register(Blob)
